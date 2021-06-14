48c04ad9715699c30b18e0cd798f9889899b301d
Diêgo Marcelino <56489559+marcelinodiego@users.noreply.github.com>
2021-06-14 20:51:21 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,61 @@
-from flask import Flask, jsonify
+import time
+from flask import Flask, jsonify, request
+from threading import Thread
+import json
+import os
+import threading
+
 
 app = Flask(__name__)
+verifica = False
 
 
 @app.route('/info')
 def get_json():
     return jsonify({
-        "componente": "resourcemanager",
-        "descricao": "serve os clientes com os servicos X, Y e Z",
-        "versao": "0.1"
+        "componente": "server",
+        "versao": "0.1",
+        "descricao": "serve os clientes com os serviços X, Y e Z",
+        "ponto_de_acesso": "https://meu-app-sd.heroku.com",
+        "status": "up",
+        "identificacao": 2,
+        "lider": 0,
+        "eleicao": "valentao",
+        "servidores_conhecidos": [
+            {
+                "id": "id_server_1",
+                "url": "url_server_1"
+            },
+            {
+                "id": "id_server2",
+                "url": "url_server2"
+            }
+        ]
     })
 
 
+@app.route('/recurso', methods=['GET', 'POST'])
+def estado():
+    global verifica
+    thr = threading.Thread(target=respFunc, args=())
+    if verifica:
+        return_code = 409
+    else:
+        verifica = True
+        thr.start()
+        return_code = 200
+    return jsonify({"ocupado": verifica}), return_code
+
+
+def respFunc():
+    global verifica
+    time.sleep(10)
+    verifica = False
+
+
 def main():
-    app.run(host='0.0.0.0', port=2003)
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port)
 
 
+main()
